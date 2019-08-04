56e463980c55c9744c926680b9a547cc76d24aef
Mubashir Reshamwala <mubresham@gmail.com>
2019-08-04 18:16:03 +0530

    port update

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
 var users = [] 
+var port = process.env.PORT || 3000
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
@@ -29,6 +30,6 @@ io.on('connection', function(socket) {
   });
 });
 
-http.listen(3000, function() {
+http.listen(port, function() {
   console.log('listening on *:3000');
 });
